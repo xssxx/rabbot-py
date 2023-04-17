009ca7d53f4b766f2bc1c407daa7bcd4bb3eaea5
koonx6520 <koonx6520@gmail.com>
2023-04-17 16:47:02 +0700

    init bot and add 3 commands

---
+++ main.py
@@ -0,0 +1,38 @@
+import discord
+import random
+import json
+
+from classes.Buttons import Buttons
+from classes.MyClient import MyClient
+
+with open("config/config.json", "r") as f:
+    config_data = json.load(f)
+    TOKEN = config_data["TOKEN"]
+    GUILD = discord.Object(config_data["GUILDS_ID"])
+
+def run():
+    # initialize
+
+    client = MyClient()
+
+    # commands
+
+    @client.tree.command(guild=GUILD, name="calculator", description="Sends an interactive calculator")
+    async def calculator(interaction: discord.Interaction):
+        await interaction.response.send_message("```Begin Calculating```", view=Buttons())
+
+    @client.tree.command(guild=GUILD, name="calc", description="command line calculator")
+    async def calc(interaction: discord.Interaction, expression: str):
+        result = str(eval(expression))
+        await interaction.response.send_message(result)
+
+    @client.tree.command(guild=GUILD, name="dice", description="roll dice")
+    async def calc(interaction: discord.Interaction, count:int = 1):
+        result = random.randint(1, 7 * count)
+        await interaction.response.send_message(content=f"```Your dice🎲 is: {result}```")
+
+    client.run(TOKEN)
+
+
+if __name__ == "__main__":
+    run()
